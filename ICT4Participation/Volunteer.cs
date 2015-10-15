e247a0a146f20a8b308ca2966b31a934d0540a77
dundy <dundy_pura_7@live.com>
2015-10-15 11:56:04 +0200

    constructor is af... getschedule moet nog geprogrammeerd worden

--- ICT4Participation/Volunteer.cs
+++ ICT4Participation/Volunteer.cs
@@ -8,16 +8,43 @@ namespace ICT4Participation
 {
    public class Volunteer : User
     {
+        //fields
+        private bool drivingLicense;
+        private string biography;
+        private string pathToVOG;
+        private string pathToPhoto;
+        
 
-        private bool DrivingLicense;
-        private string Biography;
-        private string PathToVOG;
-        private string PathToPhoto;
+        //properties
+        public bool DrivingLicense
+        { get; set; }
+
+        public string Biogragphy
+        { get; set; }
+
+        public string PathToVOG
+        { get; set; }
+
+        public string PathToPhoto
+        { get; set; }
+
+
+        public Volunteer(string name, DateTime dateOfBirth, string gender, string city, string adress, string email, string password, 
+            bool drivingLicense, string biography, string pathToVOG, string pathToPhoto)
+            : base( name , dateOfBirth , gender, city , adress , email , password)
+        {
+            this.DrivingLicense = drivingLicense;
+            this.Biogragphy = biography;
+            this.PathToVOG = pathToVOG;
+            this.PathToPhoto = pathToPhoto;
+        }
+
+
 
         public Schedule GetSchedule()
         {
             Schedule schedule = new Schedule();
-            
+            return schedule;
         }
 
         
