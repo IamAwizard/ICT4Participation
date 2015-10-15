91be1239675ad5a4d2f77d1572a92b7715ff9cbf
cptspeedy <mauriceharsveld@gmail.com>
2015-10-15 11:37:22 +0200

    hey

--- ICT4Participation/Chat.cs
+++ ICT4Participation/Chat.cs
@@ -8,8 +8,36 @@ namespace ICT4Participation
 {
     class Chat
     {
-        private User Author;
-        private DateTime Date;
-        private string Content;
+        private User author;
+        private DateTime date;
+        private string content;
+
+        public User Author
+            {
+            get { return author; }
+            set { author = value; }
+                
+            }
+        public DateTime Date
+        {
+            get { return date; }
+            set {date = value; }
+
+        }
+
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+
+        }
+
+        public Chat(User author, DateTime date,string content)
+        {
+            this.Author = this.author;
+            this.Content = this.content;
+            this.Date = this.date;
+        }
+
     }
 }
