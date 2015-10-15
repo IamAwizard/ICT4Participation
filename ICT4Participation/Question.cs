0434ddf6ddde6016870a585b99e2200680a8d2c9
cptspeedy <mauriceharsveld@gmail.com>
2015-10-15 13:05:41 +0200

    Question class is af

--- ICT4Participation/Question.cs
+++ ICT4Participation/Question.cs
@@ -8,13 +8,74 @@ namespace ICT4Participation
 {
     class Question
     {
-        private string Name;
-        private DateTime Date;
-        private string Content;
-        private string Discrepancy;
-        private string Location;
-        private int Distance;
-        private string Transport;
-        private bool Solved;
+        //fields
+        private string name;
+        private DateTime date;
+        private string content;
+        private string discrepancy;
+        private string location;
+        private int distance;
+        private string transport;
+        private bool solved;
+        //properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+        public string Discrepancy
+        {
+            get { return discrepancy; }
+            set { discrepancy = value; }
+        }
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+        public int Distance
+        {
+            get { return distance; }
+            set { distance = value; }
+        }
+
+        public string Transport
+        {
+            get { return transport; }
+            set { transport = value; }
+        }
+        public bool Solved
+        {
+            get { return solved; }
+            set { solved = value; }
+        }
+        //constructor
+        public Question(string name, string location,string transport,int distance,string Discrepancy,string content,DateTime date,bool solved)
+        {
+            this.Name = this.name;
+            this.Location = this.location;
+            this.Transport = this.transport;
+            this.Content = this.content;
+            this.Discrepancy = this.discrepancy;
+            this.Date = this.date;
+            this.Solved = this.solved;
+            this.Distance = this.distance;
+        }
+        
+        public override string ToString()
+        {
+            return "naam" + Name + "locatie" + location + "vervoersmiddel" + Transport+"afstand" +Distance + "inhoud" + Content + "Bijzonderheden" + Discrepancy + "Datum" + Date + "Geaccepteerd?" + Solved;
+        }
     }
 }
