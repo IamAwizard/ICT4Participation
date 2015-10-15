e939ba45c97cebf8b39cc48945746397b03ec20e
Jeroen Roovers <jroovers93@gmail.com>
2015-10-15 14:10:26 +0200

    Maestrocommit

---
+++ ICT4Participation/AdminHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Participation
+{
+    class AdminHandler
+    {
+        // Fields
+        Admin currentuser;
+
+        // Constructor   
+        public AdminHandler()
+        {
+
+        }
+
+        // Properties
+
+        // Methods
+        public bool DeleteQuestion(Question helprequest)
+        {
+            /// TODO: IMPLEMENT
+            return false;
+        }
+
+        public bool DeleteReview(Review review)
+        {
+            // TODO: IMPLEMENT
+            return false;
+        }
+
+        public List<Review> GetReviews()
+        {
+            // TODO: IMPLEMENT
+            return null;
+        }
+
+        public List<Question> GetQuestions()
+        {
+            // TODO: IMPLEMENT
+            return null;
+        }
+    }
+}
