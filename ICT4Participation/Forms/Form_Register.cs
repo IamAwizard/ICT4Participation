1434f149b13fb4482297b6a578fcef029bf582ed
Jeroen Roovers <jroovers93@gmail.com>
2015-10-22 12:07:24 +0200

    AAN DE KANT
    COMMIT COMING TROUGH

--- ICT4Participation/Forms/Form_Register.cs
+++ ICT4Participation/Forms/Form_Register.cs
@@ -15,6 +15,48 @@ namespace ICT4Participation
         public Form_Register()
         {
             InitializeComponent();
+
+            cbox_Gender.SelectedIndex = 0; 
+        }
+
+
+        /// <summary>
+        /// Show and or Hide VOG info based on radiobutton selection
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void rbtn_Volunteer_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rbtn_Volunteer.Checked)
+            {
+                pnl_VOGInfo.Show();
+            }
+            else
+            {
+                pnl_VOGInfo.Hide();
+            }
+        }
+
+        private User CheckAndReadEverything()
+        {
+            string email, password, naam, gender, city, address;
+            DateTime dateofbirth;
+
+            email = tbox_Email.Text;
+            if(!email.Contains('@'))
+            {
+                MessageBox.Show("Er is geen valide email ingevuld");
+                return null;
+            }
+            return null;
+        }
+
+        private void btn_VOGUpload_Click(object sender, EventArgs e)
+        {
+            if(DialogResult.OK == OpenFileDialog.ShowDialog())
+            {
+                lbl_VOGPath.Text = OpenFileDialog.FileName;
+            }
         }
     }
 }
