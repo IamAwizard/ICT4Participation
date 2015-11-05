5bcea19b796f6dacc9540c5204fa4a2344770f06
cptspeedy <gamingsessions@hotmail.com>
2015-11-05 11:28:01 +0100

    33

---
+++ ICT4Participation/Forms/Form_acceptassignment.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ICT4Participation.Forms
+{
+    partial class Form_acceptassignment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_traveltime = new System.Windows.Forms.Label();
+            this.lbl_vehicle = new System.Windows.Forms.Label();
+            this.lbl_assignment = new System.Windows.Forms.Label();
+            this.lbl_distance = new System.Windows.Forms.Label();
+            this.tb_specialty = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btn_backassignment = new System.Windows.Forms.Button();
+            this.btn_acceptassignment = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(234, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Opdracht";
+            // 
+            // lbl_traveltime
+            // 
+            this.lbl_traveltime.AutoSize = true;
+            this.lbl_traveltime.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_traveltime.ForeColor = System.Drawing.Color.White;
+            this.lbl_traveltime.Location = new System.Drawing.Point(16, 193);
+            this.lbl_traveltime.Name = "lbl_traveltime";
+            this.lbl_traveltime.Size = new System.Drawing.Size(70, 24);
+            this.lbl_traveltime.TabIndex = 3;
+            this.lbl_traveltime.Text = "Reistijd";
+            // 
+            // lbl_vehicle
+            // 
+            this.lbl_vehicle.AutoSize = true;
+            this.lbl_vehicle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_vehicle.ForeColor = System.Drawing.Color.White;
+            this.lbl_vehicle.Location = new System.Drawing.Point(14, 262);
+            this.lbl_vehicle.Name = "lbl_vehicle";
+            this.lbl_vehicle.Size = new System.Drawing.Size(139, 24);
+            this.lbl_vehicle.TabIndex = 4;
+            this.lbl_vehicle.Text = "vervoersmiddel";
+            // 
+            // lbl_assignment
+            // 
+            this.lbl_assignment.AutoSize = true;
+            this.lbl_assignment.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_assignment.ForeColor = System.Drawing.Color.White;
+            this.lbl_assignment.Location = new System.Drawing.Point(14, 114);
+            this.lbl_assignment.Name = "lbl_assignment";
+            this.lbl_assignment.Size = new System.Drawing.Size(126, 24);
+            this.lbl_assignment.TabIndex = 5;
+            this.lbl_assignment.Text = "Bijzonderheid";
+            // 
+            // lbl_distance
+            // 
+            this.lbl_distance.AutoSize = true;
+            this.lbl_distance.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_distance.ForeColor = System.Drawing.Color.White;
+            this.lbl_distance.Location = new System.Drawing.Point(16, 339);
+            this.lbl_distance.Name = "lbl_distance";
+            this.lbl_distance.Size = new System.Drawing.Size(72, 24);
+            this.lbl_distance.TabIndex = 6;
+            this.lbl_distance.Text = "Afstand";
+            // 
+            // tb_specialty
+            // 
+            this.tb_specialty.Location = new System.Drawing.Point(174, 101);
+            this.tb_specialty.Multiline = true;
+            this.tb_specialty.Name = "tb_specialty";
+            this.tb_specialty.Size = new System.Drawing.Size(274, 49);
+            this.tb_specialty.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(174, 177);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 49);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(174, 252);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(274, 49);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(174, 327);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(274, 49);
+            this.textBox3.TabIndex = 10;
+            // 
+            // btn_backassignment
+            // 
+            this.btn_backassignment.BackColor = System.Drawing.Color.White;
+            this.btn_backassignment.FlatAppearance.BorderSize = 0;
+            this.btn_backassignment.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_backassignment.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_backassignment.Location = new System.Drawing.Point(40, 447);
+            this.btn_backassignment.Name = "btn_backassignment";
+            this.btn_backassignment.Size = new System.Drawing.Size(197, 41);
+            this.btn_backassignment.TabIndex = 16;
+            this.btn_backassignment.Text = "Back";
+            this.btn_backassignment.UseVisualStyleBackColor = false;
+            // 
+            // btn_acceptassignment
+            // 
+            this.btn_acceptassignment.BackColor = System.Drawing.Color.White;
+            this.btn_acceptassignment.FlatAppearance.BorderSize = 0;
+            this.btn_acceptassignment.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_acceptassignment.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_acceptassignment.Location = new System.Drawing.Point(284, 447);
+            this.btn_acceptassignment.Name = "btn_acceptassignment";
+            this.btn_acceptassignment.Size = new System.Drawing.Size(197, 41);
+            this.btn_acceptassignment.TabIndex = 17;
+            this.btn_acceptassignment.Text = "Accepteer opdracht";
+            this.btn_acceptassignment.UseVisualStyleBackColor = false;
+            // 
+            // Form_acceptassignment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(731, 511);
+            this.Controls.Add(this.btn_acceptassignment);
+            this.Controls.Add(this.btn_backassignment);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tb_specialty);
+            this.Controls.Add(this.lbl_distance);
+            this.Controls.Add(this.lbl_assignment);
+            this.Controls.Add(this.lbl_vehicle);
+            this.Controls.Add(this.lbl_traveltime);
+            this.Controls.Add(this.label1);
+            this.Name = "Form_acceptassignment";
+            this.Text = "Form_Opdrachtaanvaarden";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_traveltime;
+        private System.Windows.Forms.Label lbl_vehicle;
+        private System.Windows.Forms.Label lbl_assignment;
+        private System.Windows.Forms.Label lbl_distance;
+        private System.Windows.Forms.TextBox tb_specialty;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btn_backassignment;
+        private System.Windows.Forms.Button btn_acceptassignment;
+    }
+}
