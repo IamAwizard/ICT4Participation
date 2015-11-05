915cf9d99b886a95369692b74856c7de6f5d9ca9
dundy <dundy_pura_7@live.com>
2015-11-05 10:35:46 +0100

    ik denk dat dit wel goed is niet ?

--- ICT4Participation/Forms/Form_MainAdmin.Designer.cs
+++ ICT4Participation/Forms/Form_MainAdmin.Designer.cs
@@ -28,19 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dg = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dg
-            // 
-            this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dg.Location = new System.Drawing.Point(189, 37);
-            this.dg.Name = "dg";
-            this.dg.Size = new System.Drawing.Size(344, 206);
-            this.dg.TabIndex = 0;
             // 
             // dataGridView2
             // 
@@ -57,19 +47,15 @@
             this.BackColor = System.Drawing.SystemColors.Highlight;
             this.ClientSize = new System.Drawing.Size(571, 422);
             this.Controls.Add(this.dataGridView2);
-            this.Controls.Add(this.dg);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form_MainAdmin";
             this.Text = "Form_MainAdmin";
-            ((System.ComponentModel.ISupportInitialize)(this.dg)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dg;
         private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
